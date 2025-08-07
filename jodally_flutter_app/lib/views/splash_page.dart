e3369f6ab6aa013b89c9b14a153f6545892c3967
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-07 19:32:59 +0800

    initial splash-page modified
    Added:
    - font
    - color
    - images
    
    Directory structure

---
+++ jodally_flutter_app/lib/views/splash_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:jodally_flutter_app/core/resources/colors.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: greenSecondary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/jeepney_splash_img.png',
+              height: 150,
+              width: 150,
+            ),
+            Text(
+              'Jodally',
+              style: TextStyle(
+                fontFamily: "Poppins",
+                color: whitePrimary,
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              '“Jeepney Rides, Made Easy!”',
+              style: TextStyle(
+                fontFamily: "Nunito",
+                color: whitePrimary,
+                fontSize: 14,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
