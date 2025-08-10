d2a5ef6150e9194f84a89862f3ce3fd46d735472
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-10 16:21:19 +0800

    implemented terms and conditions and Privacy policy

---
+++ jodally_flutter_app/lib/core/resources/app_constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  //GLOBAL
+
+  // --- TEXT FIELDS ---
+  static const String EMAIL_TITLE = 'Email';
+  static const String EMAIL_SUB_TITLE = 'Enter your email';
+  static const String PASSWORD_TITLE = 'Password';
+  static const String PASSWORD_SUB_TITLE = 'Enter your password';
+
+  //SIGN_UP_PASSENGER_AND_DRIVER
+  static const String SIGN_UP_TITLE = 'Create Account';
+  static const String SIGN_UP_DESC =
+      'Join the Jodally community today and make your \njourney.';
+}
