00570cd04ff74d95424cc5656d5c92582f8c2593
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-11 14:56:52 +0800

    misc  files

--- jodally_flutter_app/lib/main.dart
+++ jodally_flutter_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jodally_flutter_app/views/sign_up_driver.dart';
 import 'package:jodally_flutter_app/views/sign_up_passenger.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       // ),
-      home: SignUpDriver(),
+      home: SignUpPassenger(),
     );
   }
 }
