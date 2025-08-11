725ba3da6cda0c9b5c075b8fa474240684f685c1
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-11 16:16:45 +0800

    added initial widgets

--- jodally_flutter_app/lib/views/sign_up_driver_license.dart
+++ jodally_flutter_app/lib/views/sign_up_driver_license.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:jodally_flutter_app/core/components/bullet_list.dart';
 import 'package:jodally_flutter_app/core/components/common_back_app_bar.dart';
+import 'package:jodally_flutter_app/core/components/common_button.dart';
+import 'package:jodally_flutter_app/core/components/common_text_label.dart';
+import 'package:jodally_flutter_app/core/resources/app_constants.dart';
 import 'package:jodally_flutter_app/core/resources/colors.dart';
+import 'package:jodally_flutter_app/core/resources/dimensions.dart';
 
 class SignUpDriverLicense extends StatefulWidget {
   const SignUpDriverLicense({super.key});
@@ -17,7 +22,55 @@ class _SignUpDriverLicenseState extends State<SignUpDriverLicense> {
       appBar: CommonBackAppbar(onTap: () {}),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-        child: SingleChildScrollView(child: Column(children: [])),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CommonTextLabel(
+                text: 'Driver’s License',
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+                fontSize: 24,
+              ),
+              BulletList(
+                strings: [
+                  AppConstants.FIRST_BULLET,
+                  AppConstants.SECOND_BULLET,
+                  AppConstants.THIRD_BULLET,
+                ],
+              ),
+              SizedBox(height: 12),
+              CommonTextLabel(
+                text: 'Attach Driver’s License',
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.w400,
+                fontSize: fontSizeTitleSmall,
+              ),
+              SizedBox(height: 12),
+              CommonTextLabel(
+                text: 'Attach OR/CR',
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.w400,
+                fontSize: fontSizeTitleSmall,
+              ),
+              const SizedBox(height: 24),
+              CommonButton(
+                backgroundColor: greenPrimary,
+                borderRadius: 6,
+                width: double.infinity,
+                height: 50,
+                child: CommonTextLabel(
+                  text: 'Continue',
+                  fontFamily: 'Poppins',
+                  fontSize: fontSizeTitleLarge,
+                  color: whitePrimary,
+                ),
+                //onPressed: () => Navigator.of(context).pop(),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
