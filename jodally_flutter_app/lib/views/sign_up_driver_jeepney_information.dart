f1f5df13a9770f73b2c6fdab9ac2f6042739cec9
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-11 17:24:47 +0800

    Finish sign_up_driver_jeepney_information

--- jodally_flutter_app/lib/views/sign_up_driver_jeepney_information.dart
+++ jodally_flutter_app/lib/views/sign_up_driver_jeepney_information.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:jodally_flutter_app/core/components/common_back_app_bar.dart';
+import 'package:jodally_flutter_app/core/components/common_button.dart';
+import 'package:jodally_flutter_app/core/components/common_text_field.dart';
 import 'package:jodally_flutter_app/core/components/common_text_label.dart';
+import 'package:jodally_flutter_app/core/components/dashed_outline_button.dart';
+import 'package:jodally_flutter_app/core/resources/assets.dart';
 import 'package:jodally_flutter_app/core/resources/colors.dart';
 import 'package:jodally_flutter_app/core/resources/dimensions.dart';
 
@@ -39,7 +43,104 @@ class _SignUpDriverJeepneyInformationState
                 fontWeight: FontWeight.w400,
                 fontSize: fontSizeTitleSmall,
               ),
+              const SizedBox(height: 28),
+              //Owner Name Label
+              CommonTextLabel(
+                text: 'Owner Name',
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.w400,
+                fontSize: fontSizeTitleSmall,
+                color: blackTertiary,
+              ),
+              const SizedBox(height: 4),
+              // Owner Name Field
+              CommonTextField(
+                filled: true,
+                fillColor: lightGreyPrimary,
+                hintText: 'Enter owner name',
+                hintTextStyle: TextStyle(
+                  fontFamily: "Nunito",
+                  fontWeight: FontWeight.w400,
+                  fontSize: fontSizeSubhead,
+                  color: lightGreySecondary,
+                ),
+                prefixIcon: Image.asset(
+                  Assets.smsIcon,
+                  scale: 3,
+                  color: lightGreySecondary,
+                ),
+                onFieldSubmitted: (_) => FocusScope.of(context).unfocus(),
+                keyboardType: TextInputType.emailAddress,
+                inputBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                  borderSide: BorderSide(color: lightGreyPrimary, width: 1),
+                ),
+              ),
               const SizedBox(height: 12),
+              //Plate Number Label
+              CommonTextLabel(
+                text: 'Plate Number',
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.w400,
+                fontSize: fontSizeTitleSmall,
+                color: blackTertiary,
+              ),
+              const SizedBox(height: 4),
+              // Owner Name Field
+              CommonTextField(
+                filled: true,
+                fillColor: lightGreyPrimary,
+                hintText: 'Enter your plate number',
+                hintTextStyle: TextStyle(
+                  fontFamily: "Nunito",
+                  fontWeight: FontWeight.w400,
+                  fontSize: fontSizeSubhead,
+                  color: lightGreySecondary,
+                ),
+                prefixIcon: Image.asset(
+                  Assets.smsIcon,
+                  scale: 3,
+                  color: lightGreySecondary,
+                ),
+                onFieldSubmitted: (_) => FocusScope.of(context).unfocus(),
+                keyboardType: TextInputType.emailAddress,
+                inputBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                  borderSide: BorderSide(color: lightGreyPrimary, width: 1),
+                ),
+              ),
+              const SizedBox(height: 12),
+              //Attach Jeepney Label
+              CommonTextLabel(
+                text: 'Attach Jeepney',
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.w400,
+                fontSize: fontSizeTitleSmall,
+                color: blackTertiary,
+              ),
+              SizedBox(height: 12),
+              DashedOutlineButton(
+                label: 'Upload',
+                onPressed: () {
+                  // Handle button press
+                },
+              ),
+              const SizedBox(height: 30),
+              //Continue Button
+              CommonButton(
+                backgroundColor: greenPrimary,
+                borderRadius: 6,
+                width: double.infinity,
+                height: 50,
+                child: CommonTextLabel(
+                  text: 'Sign Up',
+                  fontFamily: 'Poppins',
+                  fontSize: fontSizeTitleLarge,
+                  color: whitePrimary,
+                ),
+                //onPressed: () => Navigator.of(context).pop(),
+                onPressed: () {},
+              ),
             ],
           ),
         ),
