3c0b718ccb7339e44eb6101be1c8241e78a11d02
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-12 20:10:22 +0800

    added AnnotatedRegion on Verification Email

--- jodally_flutter_app/lib/views/verification_page.dart
+++ jodally_flutter_app/lib/views/verification_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:jodally_flutter_app/core/components/common_button.dart';
 import 'package:jodally_flutter_app/core/components/common_text_label.dart';
 import 'package:jodally_flutter_app/core/resources/app_constants.dart';
@@ -16,48 +17,54 @@ class VerificationPage extends StatefulWidget {
 class _VerificationPageState extends State<VerificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(Assets.varificationImg, scale: 4),
-              CommonTextLabel(
-                text: 'Verify Your Email',
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w600,
-                fontSize: fontSizeTitleLarge,
-                color: blackPrimary,
-              ),
-              SizedBox(height: 8),
-              SizedBox(
-                //width: 313,
-                child: CommonTextLabel(
-                  text: AppConstants.VERIFICATION_DESC,
-                  fontFamily: 'Nunito',
-                  fontWeight: FontWeight.w400,
-                  fontSize: fontSizeTitleSmall,
-                  color: greyPrimary,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              SizedBox(height: 48),
-              CommonButton(
-                backgroundColor: greenPrimary,
-                borderRadius: 6,
-                width: double.infinity,
-                height: 50,
-                child: CommonTextLabel(
-                  text: 'Close',
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+        statusBarColor: Colors.transparent,
+      ),
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(Assets.varificationImg, scale: 4),
+                CommonTextLabel(
+                  text: 'Verify Your Email',
                   fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
                   fontSize: fontSizeTitleLarge,
-                  color: whitePrimary,
+                  color: blackPrimary,
                 ),
-                onPressed: () {},
-              ),
-            ],
+                SizedBox(height: 8),
+                SizedBox(
+                  //width: 313,
+                  child: CommonTextLabel(
+                    text: AppConstants.VERIFICATION_DESC,
+                    fontFamily: 'Nunito',
+                    fontWeight: FontWeight.w400,
+                    fontSize: fontSizeTitleSmall,
+                    color: greyPrimary,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 48),
+                CommonButton(
+                  backgroundColor: greenPrimary,
+                  borderRadius: 6,
+                  width: double.infinity,
+                  height: 50,
+                  child: CommonTextLabel(
+                    text: 'Close',
+                    fontFamily: 'Poppins',
+                    fontSize: fontSizeTitleLarge,
+                    color: whitePrimary,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
