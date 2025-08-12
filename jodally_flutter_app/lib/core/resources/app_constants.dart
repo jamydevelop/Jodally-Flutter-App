4d21c69e73ce05eeb740c828339dfc6e3b0a25cb
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-12 19:25:13 +0800

    Verification Page done

--- jodally_flutter_app/lib/core/resources/app_constants.dart
+++ jodally_flutter_app/lib/core/resources/app_constants.dart
@@ -18,4 +18,8 @@ class AppConstants {
       'The Photo and all details must be clearly visible.';
   static const String THIRD_BULLET =
       'Only documents that are less than 10 MB in size and in JPEG or PNG will be accepted.';
+
+  //VERIFIVATION_PAGE
+  static const String VERIFICATION_DESC =
+      "We've sent a verification email to kentrobert@gmail.com. Please check your inbox and verify your email before logging in.";
 }
