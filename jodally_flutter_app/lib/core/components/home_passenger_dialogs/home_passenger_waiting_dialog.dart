7795138445861e1e8c2720a359926813658998e7
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-08-21 19:29:51 +0800

    Inital waiting home passenger dialog

---
+++ jodally_flutter_app/lib/core/components/home_passenger_dialogs/home_passenger_waiting_dialog.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePassengerWaitingDialog extends StatefulWidget {
+  const HomePassengerWaitingDialog({super.key});
+
+  @override
+  State<HomePassengerWaitingDialog> createState() =>
+      _HomePassengerWaitingDialogState();
+}
+
+class _HomePassengerWaitingDialogState
+    extends State<HomePassengerWaitingDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
